c5f585b4b3c3e2d1b629cda2f60768f7f739ea17
chueunjeong <chuitchew@gmail.com>
2023-05-20 11:08:00 +0900

    2023-05-20

---
+++ leetcode/J189.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class J189 {
+
+  public static void main(String[] args) {
+    int[] arr1 = { 1, 2, 3, 4, 5, 6, 7 };
+    int k1 = 3;
+    int[] arr2 = { -1, -100, 3, 99 };
+    int k2 = 2;
+    rotate(arr2, k2);
+  }
+
+  public static void rotate(int[] nums, int k) {
+    int[] numsCopy = nums.clone();
+    for (int i = 0; i < numsCopy.length; i++) {
+      nums[(i + k) % nums.length] = numsCopy[i];
+    }
+
+    System.out.println(Arrays.toString(nums));
+  }
+
+  public class Node {
+
+    int index;
+    int num;
+
+    public Node(int index, int num) {
+      this.index = index;
+      this.num = num;
+    }
+  }
+}
