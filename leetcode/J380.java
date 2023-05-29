59759d530e8774bd29c3c6712b8a742fe0824ea8
chueunjeong <chuitchew@gmail.com>
2023-05-29 15:27:01 +0900

    2023-05-29

---
+++ leetcode/J380.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class J380 {
+
+  public static void main(String[] args) {
+    RandomizedSet randomizedSet = new RandomizedSet();
+    System.out.println(randomizedSet.insert(1)); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+    System.out.println(randomizedSet.remove(2)); // Returns false as 2 does not exist in the set.
+    System.out.println(randomizedSet.insert(2)); // Inserts 2 to the set, returns true. Set now contains [1,2].
+    System.out.println(randomizedSet.getRandom()); // getRandom() should return either 1 or 2 randomly.
+    System.out.println(randomizedSet.remove(1)); // Removes 1 from the set, returns true. Set now contains [2].
+    System.out.println(randomizedSet.insert(2)); // 2 was already in the set, so return false.
+    System.out.println(randomizedSet.getRandom()); // Since 2 is the only number in the set, getRandom() will always return 2.
+  }
+}
+
+class RandomizedSet {
+
+  private HashMap<Integer, Boolean> hashMap;
+
+  public RandomizedSet() {
+    this.hashMap = new HashMap<Integer, Boolean>();
+  }
+
+  public boolean insert(int val) {
+    return hashMap.putIfAbsent(val, true) == null ? true : false;
+  }
+
+  public boolean remove(int val) {
+    return hashMap.remove(val) != null ? true : false;
+  }
+
+  public int getRandom() {
+    ArrayList<Integer> keySet = new ArrayList<>(hashMap.keySet());
+    int randomNum = ThreadLocalRandom.current().nextInt(0, keySet.size());
+    return keySet.get(randomNum);
+  }
+}
