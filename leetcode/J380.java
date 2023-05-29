41a1d9c8deba9fd256d8908681109e439df179ae
chueunjeong <chuitchew@gmail.com>
2023-05-29 15:46:27 +0900

    2023-05-29

--- leetcode/J380.java
+++ leetcode/J380.java
@@ -22,18 +22,22 @@ class RandomizedSet {
 
   private HashMap<Integer, Boolean> hashMap;
 
+  //주어진 숫자를 넣기위해 HashMap 초기화
   public RandomizedSet() {
     this.hashMap = new HashMap<Integer, Boolean>();
   }
 
+  //putIfAbsent()은 해당 값이 없으면 null, 있으면 해당 값을 반환
   public boolean insert(int val) {
     return hashMap.putIfAbsent(val, true) == null ? true : false;
   }
 
+  //remove()는 해당 값이 있으면 true, 없으면 null 반환
   public boolean remove(int val) {
     return hashMap.remove(val) != null ? true : false;
   }
 
+  //0~(숫자 개수-1) 사이의 숫자를 랜덤으로 생성하여 그 인덱스에 해당하는 값 반환
   public int getRandom() {
     ArrayList<Integer> keySet = new ArrayList<>(hashMap.keySet());
     int randomNum = ThreadLocalRandom.current().nextInt(0, keySet.size());
