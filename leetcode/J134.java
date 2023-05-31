058d2049f8e673de69f5a6355b4e7581a7cebf25
chueunjeong <chuitchew@gmail.com>
2023-05-31 21:50:50 +0900

    2023-05-31

--- leetcode/J134.java
+++ leetcode/J134.java
@@ -18,12 +18,16 @@ public class J134 {
   public static int canCompleteCircuit(int[] gas, int[] cost) {
     int[] movePrices = new int[gas.length];
 
+    //미리 그 지점에서 획득량-소요량을 계산해둔다.
     for (int i = 0; i < gas.length; i++) {
       movePrices[i] = gas[i] - cost[i];
     }
 
+    //총 획득량-총 소요량을 저장
     int total = 0;
+    //시작점 기록
     int answer = 0;
+    //각 시작점에 남은 가스량 기록
     int sum = 0;
     for (int k = 0; k < movePrices.length; k++) {
       sum += movePrices[k % movePrices.length];
@@ -38,8 +42,10 @@ public class J134 {
         answer = k + 1;
       }
     }
+    //맞는 시작지점이면 다시 시작하지 않고 가스를 가지고 있기때문에, total에 더해줘
+    //total이 음수인지 확인한다.
+    //음수이면 반복문을 다 돌아도 가스가 부족한 상황이다.
     total += sum;
-    System.out.println(total);
     return total < 0 ? -1 : answer;
   }
 }
